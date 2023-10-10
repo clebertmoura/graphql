61a7716dc43b59cd63b1749aab68883cf610d1bf
Cleber Moura <ctm@cin.ufpe.br>
2023-10-09 22:10:14 -0300

    first commit

---
+++ src/main/java/br/com/ctm/graphql/BookController.java
@@ -0,0 +1,22 @@
+package br.com.ctm.graphql;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import br.com.ctm.graphql.domain.Author;
+import br.com.ctm.graphql.domain.Book;
+
+@Controller
+public class BookController {
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.authorId());
+    }
+}
